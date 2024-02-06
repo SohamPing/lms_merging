30bdcf58a1434cf2625de6392c3c2b3610aaecea
Soham Pingat <soham.pingat@gmail.com>
2024-02-06 15:56:14 -0500

    Added ES6 and ES Modules to the Project

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+import express from "express"
+import path from "path"
+import bodyParser from "body-parser"
+import ejs from "ejs"
 const app = express();
 
 
-
-// Import the functions you need from the SDKs you need
-var firebase = require("firebase/app");
-require("firebase/auth");
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyDaBRb4kvKkbyzonhe4OgMusvguc0asuaI",
   authDomain: "lmsproject-1f5c0.firebaseapp.com",
@@ -21,27 +15,41 @@ const firebaseConfig = {
   messagingSenderId: "334848584616",
   appId: "1:334848584616:web:4dbc95b5479edb8093cb9d"
 };
+const FirebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(FirebaseApp);
 
-// Initialize Firebase
-const FirebaseApp = firebase.initializeApp(firebaseConfig);
-
-
-
+const staticPath = path.join(process.cwd(), 'public');
+app.use(express.static(staticPath));
 app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
-app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Routes setup
 // For example, rendering the login page
 app.route("/login")
         .get(function (req,res) {
-            res.render('login')
-});
+            res.render('login')})
+            .post(function (req,res){
+              var email,password;
+              email = req.body.loginemail;
+              password = req.body.password;
+             
+              signInWithEmailAndPassword(auth, email, password)
+              .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                // ...
+                res.redirect('/Dashboard/id='+userCredential.user.uid);
+              })
+              .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                res.send('<center><h2>'+errorMessage+'</h2></center>');
+              })
+            });
 
-app.get('/', (req, res) => {
+app.get('/Dashboard', (req, res) => {
     // Send a simple text response
     res.send('Hello, world!');
   });
