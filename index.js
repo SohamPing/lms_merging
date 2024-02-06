4ddc8658e0813762abc5aeb736f7f83cf8537f30
Soham Pingat <soham.pingat@gmail.com>
2024-02-05 19:14:29 -0500

    can't add firebase, someone please fix it

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const app = express();
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaBRb4kvKkbyzonhe4OgMusvguc0asuaI",
+  authDomain: "lmsproject-1f5c0.firebaseapp.com",
+  projectId: "lmsproject-1f5c0",
+  storageBucket: "lmsproject-1f5c0.appspot.com",
+  messagingSenderId: "334848584616",
+  appId: "1:334848584616:web:4dbc95b5479edb8093cb9d"
+};
+
+// Initialize Firebase
+const FirebaseApp = initializeApp(firebaseConfig);
+
+
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(express.static("public"));
+
+
+// Routes setup
+// For example, rendering the login page
+app.route("/login")
+        .get(function (req,res) {
+            res.render('login')
+});
 
 app.get('/', (req, res) => {
     // Send a simple text response
